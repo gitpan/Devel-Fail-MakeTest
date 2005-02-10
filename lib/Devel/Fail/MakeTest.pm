f574b438357a92b28fc1d4a60a2daf2c29ee28d4
Martin 'Kingpin' Thurn <kingpin@dcswcc.org>
2005-02-10 11:44:25 +0000

    Import of MTHURN/Devel-Fail-MakeTest-1.004 from CPAN.
    gitpan-cpan-distribution: Devel-Fail-MakeTest
    gitpan-cpan-version:      1.004
    gitpan-cpan-path:         MTHURN/Devel-Fail-MakeTest-1.004.tar.gz
    gitpan-cpan-author:       MTHURN
    gitpan-cpan-maturity:     released

---
+++ lib/Devel/Fail/MakeTest.pm
@@ -0,0 +1,8 @@
+# This dummy/empty module exists only so that it gets indexed in the
+# CPAN module list.
+package Devel::Fail::MakeTest;
+my
+$VERSION = do { my @r = (q$Revision: 1.3 $ =~ /\d+/g); sprintf "%d."."%03d" x $#r, @r };
+1;
+__END__
+
